1fb1f8663f1ba869fbb74c85810ac0a1eab87e7b
hector.hurtado <hector.hurtado@ceiba.com.co>
2021-09-27 16:20:27 -0500

    delete clases usuario

--- microservicio/aplicacion/src/main/java/com/ceiba/movimiento/consulta/ManejadorObtenerGananciaMovimientos.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/movimiento/consulta/ManejadorObtenerGananciaMovimientos.java
@@ -29,7 +29,7 @@ public class ManejadorObtenerGananciaMovimientos {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.set(Calendar.HOUR_OF_DAY,0);
-        calendar.set(Calendar.MINUTE,00);
+        calendar.set(Calendar.MINUTE,0);
         calendar.set(Calendar.SECOND,0);
         calendar.set(Calendar.MILLISECOND,0);
 
